42eaeb1d0a7b6d0b561fb756128ef8c2ce155425
guri11 <32800656+guri11@users.noreply.github.com>
2017-10-16 22:30:52 +0530

    Update media.py

--- media.py
+++ media.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
 import webbrowser
 
 
@@ -9,13 +7,7 @@ class Movie:
 
     VALID_RATINGS = ['G', 'PG', 'PG-13', 'R']
 
-    def __init__(
-        self,
-        movie_title,
-        movie_storyline,
-        poster_image_url,
-        trailer_youtube,
-         ):
+    def __init__(self, movie_title, movie_storyline, poster_image_url, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image_url
